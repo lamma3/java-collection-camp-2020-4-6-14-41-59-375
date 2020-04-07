f347280026b86e543775c4ddb9688d707befff44
Marco Lam <lamma3@oocl.com>
2020-04-07 12:00:36 +0800

    add Map implementation

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -1,9 +1,9 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
@@ -17,18 +17,26 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return array.stream()
+                .map(num -> num * 3)
+                .collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return array.stream()
+                .map(num -> letters[num-1])
+                .collect(Collectors.toList());
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        return array.stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return array.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
